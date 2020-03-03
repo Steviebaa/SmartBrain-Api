7afcf4dcba5f2aca63f2ca401e3670845f041bf0
Steve Richardson <Steve.Richardson@lindsaydynan.com>
2020-03-03 19:48:05 +1100

    changed port to env.PORT

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 // GET IMAGEURL
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3001, () => { console.log('app is running on port 3001') })
+app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`)})
